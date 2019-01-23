95230f0bd5c453baadf90dbd4950ffb9954f75df
yeweipeng <854526102@qq.com>
2019-01-23 23:44:04 +0800

    go基础-指针v2

--- gopoint.go
+++ gopoint.go
@@ -7,8 +7,17 @@ func main() {
 	p = 10
 	changeValue(&p)
 	fmt.Println(p)
+
+	a, b := 3, 4
+	swapValue(&a, &b)
+	fmt.Println(a)
+	fmt.Println(b)
 }
 
 func changeValue(p *int) {
 	*p++
+}
+
+func swapValue(a, b *int) {
+	*a, *b = *b, *a
 }
