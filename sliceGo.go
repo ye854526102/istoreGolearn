b04861e0f8bad05ddca24fe7be77ce3d1a0d39a0
yeweipeng <854526102@qq.com>
2019-01-26 13:30:14 +0800

    新增slice的len,cap,扩展,已经超出cap时候go自己重新开辟空间创建数组

--- sliceGo.go
+++ sliceGo.go
@@ -29,6 +29,9 @@ func main() {
 	updateSlice(ss2)
 	fmt.Println("arr[:] is =", ss2)
 	fmt.Println("arr is =", arr)
+
+	fmt.Println("reslice")
+	sliceAction()
 }
 
 func updateSlice(s []int) {
@@ -40,4 +43,47 @@ func printArray(s []int) {
 	for _, v := range s {
 		fmt.Println(v)
 	}
+}
+
+//slice 的操作
+func sliceAction() {
+	//slice 再次slice
+	arr := [...]string{"go语言", "php语言", "java语言", "js语言", "c语言", "c++语言"}
+
+	s := arr[:5]
+
+	fmt.Println(s)
+
+	s_r_s := s[:2]
+
+	fmt.Println(s_r_s)
+
+	//slice的扩展
+
+	s_p := arr[2:4]
+	fmt.Println(s_p)
+
+	//查出实体的arr,s_p[1:5]会报错,最多到[1,4]
+	s_p_s := s_p[1:4]
+
+	fmt.Println(s_p_s)
+
+	fmt.Printf("%v--len(s)=%d--cap(s)=%d", s_p, len(s_p), cap(s_p))
+
+	//如果利用append函数对slice添加元素,如果超出课cap的长度,go则会自动开辟一个新空间,生成一个新的数组
+
+	a_s := arr[:len(arr)-2]
+	fmt.Println(a_s)
+
+	a_s_p1 := append(a_s, "python语言")
+	fmt.Println(a_s_p1)
+
+	a_s_p2 := append(a_s_p1, "ruby语言")
+	fmt.Println(a_s_p2)
+	fmt.Println("now arr is", arr)
+	//这个不是对arr的视图
+	a_s_p3 := append(a_s_p2, "R语言")
+	fmt.Println(a_s_p3)
+	fmt.Println("now arr is", arr)
+
 }
